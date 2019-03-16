8ef4e1a4d7dd2c0b767405bc98a0fa75df1061af
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2019-03-16 18:00:15 +0000

    Updated fetch.sh
    I added the ability to download gridpoint files too.

--- fetch.sh
+++ fetch.sh
@@ -4,13 +4,15 @@
 USERNAME=pi
 REMOTE=head.local
 DIR=/mnt/autumn/openifs-cy38r1/run
-EXPID=h3b6
+EXPID=h467
 
 # Counter for time steps
 NUM=000000
 
 # Final time step to copy
-FINAL=000120
+FINAL=002350
+
+download_gg=false
 
 while [ 1 ]
 do
@@ -22,12 +24,23 @@ do
     # Download file if it exists
     if ssh -t $USERNAME@$REMOTE "[ -f $DIR/ICMSH$EXPID+$NUM ]"
     then
-        echo "Downloading ICMSH$EXPID+$NUM"
+        echo "Downloading $NUM"
         scp $USERNAME@$REMOTE:$DIR/ICMSH$EXPID+$NUM .
+        if [ "$download_gg" = true ]; then
+            scp $USERNAME@$REMOTE:$DIR/ICMGG$EXPID+$NUM .
+        fi
 
         # Convert to NetCDF
         grib_copy ICMSH$EXPID+$NUM ICMSH$EXPID+${NUM}_[typeOfLevel]
-        cdo -t ecmwf -f nc copy -sp2gpl ICMSHh3b6+${NUM}_isobaricInhPa ICMSHh3b6+$NUM.nc
+        cdo -t ecmwf -f nc copy -sp2gpl ICMSH$EXPID+${NUM}_isobaricInhPa ICMSH$EXPID+$NUM.nc
+        rm ICMSH$EXPID+$NUM ICMSH$EXPID+${NUM}_isobaricInhPa
+
+        if [ "$download_gg" = true ]; then
+            cdo -R copy ICMGG$EXPID+$NUM ICMGG$EXPID+${NUM}_2
+            grib_to_netcdf -o ICMGG$EXPID+$NUM.nc ICMGG$EXPID+${NUM}_2
+            rm ICMGG$EXPID+$NUM ICMGG$EXPID+${NUM}_2
+        fi
+
         NUM=$(printf %06d "$((10#$NUM + 1))")
     # Else sleep for a second
     else
